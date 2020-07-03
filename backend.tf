a53e50caa3869c53dcecdb55d55bc27e2705385e
Jimmy Sawczuk <me@jimmysawczuk.com>
2020-07-02 21:08:19 -0400

    Upgrade to 0.12 syntax (#1)
    * Upgrade to 0.12 syntax
    
    * Formatting

--- backend.tf
+++ backend.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 0.12"
+
   backend "s3" {
     bucket  = "terraform"
     key     = "terraform.tfstate"
--- network.tf
+++ network.tf
@@ -3,31 +3,31 @@ resource "aws_vpc" "app-vpc" {
 }
 
 resource "aws_subnet" "public" {
-  vpc_id     = "${aws_vpc.app-vpc.id}"
+  vpc_id     = aws_vpc.app-vpc.id
   cidr_block = "10.0.1.0/24"
 }
 
 resource "aws_subnet" "private" {
-  vpc_id     = "${aws_vpc.app-vpc.id}"
+  vpc_id     = aws_vpc.app-vpc.id
   cidr_block = "10.0.2.0/24"
 }
 
 resource "aws_route_table" "public" {
-  vpc_id = "${aws_vpc.app-vpc.id}"
+  vpc_id = aws_vpc.app-vpc.id
 }
 
 resource "aws_route_table" "private" {
-  vpc_id = "${aws_vpc.app-vpc.id}"
+  vpc_id = aws_vpc.app-vpc.id
 }
 
 resource "aws_route_table_association" "public_subnet" {
-  subnet_id      = "${aws_subnet.public.id}"
-  route_table_id = "${aws_route_table.public.id}"
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private_subnet" {
-  subnet_id      = "${aws_subnet.private.id}"
-  route_table_id = "${aws_route_table.private.id}"
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
 }
 
 resource "aws_eip" "nat" {
@@ -35,34 +35,32 @@ resource "aws_eip" "nat" {
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = "${aws_vpc.app-vpc.id}"
+  vpc_id = aws_vpc.app-vpc.id
 }
 
 resource "aws_nat_gateway" "ngw" {
-  subnet_id     = "${aws_subnet.public.id}"
-  allocation_id = "${aws_eip.nat.id}"
+  subnet_id     = aws_subnet.public.id
+  allocation_id = aws_eip.nat.id
 
-  depends_on = [
-    "aws_internet_gateway.igw"
-  ]
+  depends_on = [aws_internet_gateway.igw]
 }
 
 resource "aws_route" "public_igw" {
-  route_table_id         = "${aws_route_table.public.id}"
+  route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.igw.id}"
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
 resource "aws_route" "private_ngw" {
-  route_table_id         = "${aws_route_table.private.id}"
+  route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.ngw.id}"
+  nat_gateway_id         = aws_nat_gateway.ngw.id
 }
 
 resource "aws_security_group" "http" {
   name        = "http"
   description = "HTTP traffic"
-  vpc_id      = "${aws_vpc.app-vpc.id}"
+  vpc_id      = aws_vpc.app-vpc.id
 
   ingress {
     from_port   = 80
@@ -75,7 +73,7 @@ resource "aws_security_group" "http" {
 resource "aws_security_group" "https" {
   name        = "https"
   description = "HTTPS traffic"
-  vpc_id      = "${aws_vpc.app-vpc.id}"
+  vpc_id      = aws_vpc.app-vpc.id
 
   ingress {
     from_port   = 443
@@ -88,7 +86,7 @@ resource "aws_security_group" "https" {
 resource "aws_security_group" "egress-all" {
   name        = "egress_all"
   description = "Allow all outbound traffic"
-  vpc_id      = "${aws_vpc.app-vpc.id}"
+  vpc_id      = aws_vpc.app-vpc.id
 
   egress {
     from_port   = 0
@@ -101,7 +99,7 @@ resource "aws_security_group" "egress-all" {
 resource "aws_security_group" "api-ingress" {
   name        = "api_ingress"
   description = "Allow ingress to API"
-  vpc_id      = "${aws_vpc.app-vpc.id}"
+  vpc_id      = aws_vpc.app-vpc.id
 
   ingress {
     from_port   = 3000
@@ -112,13 +110,13 @@ resource "aws_security_group" "api-ingress" {
 }
 
 output "vpc_id" {
-  value = "${aws_vpc.app-vpc.id}"
+  value = aws_vpc.app-vpc.id
 }
 
 output "public_subnet_id" {
-  value = "${aws_subnet.public.id}"
+  value = aws_subnet.public.id
 }
 
 output "private_subnet_id" {
-  value = "${aws_subnet.private.id}"
+  value = aws_subnet.private.id
 }
