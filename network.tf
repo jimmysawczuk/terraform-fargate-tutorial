b6099da4970a18b32c4b337ddd64ce191b61aada
Jimmy Sawczuk <me@jimmysawczuk.com>
2021-12-11 12:50:12 -0500

    Cleanup

--- network.tf
+++ network.tf
@@ -154,15 +154,3 @@ resource "aws_security_group" "ingress_api" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-output "vpc_id" {
-  value = aws_vpc.app_vpc.id
-}
-
-# output "public_subnet_id" {
-#   value = aws_subnet.public.id
-# }
-
-# output "private_subnet_id" {
-#   value = aws_subnet.private.id
-# }
